5a22adf39dc840d732a4aacbd361960875f4e6b9
Lisa Williams <lisaismyname0@gmail.com>
2022-06-16 11:49:02 -0400

    overly functional stop word eraser

--- word_frequency.py
+++ word_frequency.py
@@ -12,39 +12,29 @@ def print_word_freq(file):
     print(f"Your file is: {file}")
     with open(file) as open_file:
         read_file = open_file.read()
-    print(type(read_file))
+    for word in STOP_WORDS:
+        if word in read_file:
+            read_file = read_file.replace(f"{word}", "")
+    print(read_file)
     # omit stop words around here
-    lowercase = read_file.lower()
-    # print(lowercase)
+    """lowercase = read_file.lower()
     lowercase_without_periods = lowercase.replace(".", "")
-    # print(lowercase_without_periods)
     lowercase_without_commas = lowercase_without_periods.replace(",", "")
-    # print(lowercase_without_commas)
     lowercase_without_question = lowercase_without_commas.replace("?", "")
-    # print(lowercase_without_question)
     lowercase_no_hyphen = lowercase_without_question.replace(
         "-", " ")
-    # print(lowercase_no_hyphen)
-    # lowercase_no_n = lowercase_no_hyphen.replace()
-    # print(lowercase_no_n)
     text_split = lowercase_no_hyphen.split()
-    print(text_split)
     text_dictionary = dict.fromkeys(text_split, 0)
     print(text_dictionary)
     for word in text_split:
         text_dictionary[word] += 1
-    print(text_dictionary)
+    print(text_dictionary)"""
 
 
 """    words = text_dictionary.keys()
     print(words)
     frequency = text_dictionary.values()
     print(frequency)"""
-
-
-def omit_stop_words(dictionary):
-    for words in STOP_WORDS:
-        print(words)
 
 
 if __name__ == "__main__":
