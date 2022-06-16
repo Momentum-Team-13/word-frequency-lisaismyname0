ecdd42ac51d730a6e5ea3719590de3dde5890e30
Lisa Williams <lisaismyname0@gmail.com>
2022-06-15 20:08:36 -0400

    words are keys with value of 0

--- word_frequency.py
+++ word_frequency.py
@@ -1,8 +1,12 @@
+from pydoc import text
+
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
+
 
 def print_word_freq(file):
     print(f"Your file is: {file}")
@@ -15,6 +19,29 @@ def print_word_freq(file):
     print(type(read_file))
     print(type(file_as_string))
     print(len(file_as_string))
+    lowercase = file_as_string.lower()
+    print(lowercase)
+    lowercase_without_periods = lowercase.replace(".", "")
+    print(lowercase_without_periods)
+    lowercase_without_commas = lowercase_without_periods.replace(",", "")
+    print(lowercase_without_commas)
+    lowercase_without_question = lowercase_without_commas.replace("?", "")
+    print(lowercase_without_question)
+    lowercase_no_hyphen = lowercase_without_question.replace(
+        "-", " ")
+    print(lowercase_no_hyphen)
+    lowercase_no_apostrophe = lowercase_no_hyphen.replace("'", " ")
+    print(lowercase_no_apostrophe)
+    text_split = lowercase_no_apostrophe.split(" ")
+    print(text_split)
+    text_dictionary = dict.fromkeys(text_split, "0")
+    print(text_dictionary)
+
+
+"""    final_text = [text_split]
+    print(final_text)
+    final_text_dict = {word: "0" for word in final_text}
+    print(final_text_dict)"""
 
 
 def omit_stop_words(dictionary):
@@ -22,7 +49,7 @@ def omit_stop_words(dictionary):
         print(words)
 
 
-#omit_stop_words("") 
+# omit_stop_words("")
 # why does calling this here print the words at the top of the console?
 
 if __name__ == "__main__":
