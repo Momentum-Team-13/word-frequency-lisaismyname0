2e7e55567cd2eceb7022e6b182eaa394ae3dd914
Lisa Williams <lisaismyname0@gmail.com>
2022-06-17 10:22:48 -0400

    string splits into letters, not words

--- word_frequency.py
+++ word_frequency.py
@@ -13,15 +13,15 @@ def print_word_freq(file):
     print(f"Your file is: {file}")
     with open(file) as open_file:
         read_file = open_file.read()
-    new_file = []
     ommited_words = []
-
+    new_file = []
     for word in STOP_WORDS:
         word = word.split('\n')
         ommited_words.append(word[0])
-        print(word)
     for word in read_file:
-        print(word)
+        word = word.split("\n")
+        if word not in STOP_WORDS:
+            new_file.append(word[0])
     print(new_file)
     print(ommited_words)
     p = [t for t in new_file if t not in STOP_WORDS]
@@ -32,18 +32,11 @@ def print_word_freq(file):
     lowercase_without_punct = lowercase_without_punct1.replace(
         "?", "").replace("-", "")
     text_split = lowercase_without_punct.split()
-
     text_dictionary = dict.fromkeys(text_split, 0)
     for word in text_split:
         text_dictionary[word] += 1
     alphabetized = dict(sorted(text_dictionary.items()))
-    return alphabetized
-
-
-"""    words = text_dictionary.keys()
-    print(words)
-    frequency = text_dictionary.values()
-    print(frequency)"""
+    print(alphabetized)
 
 
 if __name__ == "__main__":
